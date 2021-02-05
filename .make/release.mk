495a24fd21b545a361a0031bbd043231b3b96560
Adriaan de Beer <>
2021-02-05 10:49:59 +0200

    ST-622 reverted to simple push as Marvin

--- .make/release.mk
+++ .make/release.mk
@@ -97,10 +97,5 @@ delete-tag: .release
 release-skampi-if-no-error: .release
 	@. $(RELEASE_SUPPORT) ; releaseSKAMPIIfNoError $$USERNAME
 
-fetch-and-pull-master:
-	git remote add origin-push $(git config remote.origin.url) \
-    git fetch origin-push \
-    git pull origin-push master
-
-commit-and-push-to-master: config-git fetch-and-pull-master
+commit-and-push-to-master: config-git
 	@. $(RELEASE_SUPPORT) ; gitCommitPush $$USERNAME
